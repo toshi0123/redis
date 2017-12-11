947593540187626e98e9d12fe5b83a77f894f5e5
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-11 13:54:21 +0900

    Added redis-server options

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 chown -R redis .
-exec su-exec redis "$@"
+exec su-exec redis redis-server \
+  --bind `ip route | grep -v default | awk '/eth0/{print $1}'` \
+  --dir ./ \
+  --save 900 1 \
+  --save 300 10 \
+  --save 60 10000
